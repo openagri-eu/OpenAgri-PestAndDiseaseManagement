900511990c3e79790b14968280b65fafcb7a54a0
Stefan Drobic <stefan.drobic@vizlore.com>
2024-07-29 18:13:20 +0200

    testing upload csv file api

--- app/api/api_v1/endpoints/data.py
+++ app/api/api_v1/endpoints/data.py
@@ -7,23 +7,30 @@ from api import deps
 from models.data import Data
 from schemas import Message
 
+from csv import reader
+from codecs import iterdecode
+
 router = APIRouter()
 
 
 @router.post("/upload/", response_model=Message)
 async def upload(
-        csv: UploadFile = File(...),
+        csv_file: UploadFile = File(...),
         db: Session = Depends(deps.get_db)
 ) -> Message:
     """
     Upload a .csv file
     """
 
-    db.query(Data).first()
-    
+    csv_reader = reader(iterdecode(csv_file.file, "utf-8"), delimiter=",")
+    imr = 0
+    for row in csv_reader:
+        for col in row:
+            print(type(col))
+            print(col)
 
-
-
-
+        imr += 1
+        if imr == 2:
+            break
 
     return Message(message="Successfully uploaded file to the database.")
