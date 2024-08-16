68276bb178067a88db9575be8136db96490981a8
stefan.drobic <stefan.drobic@vizlore.com>
2024-08-16 17:41:28 +0200

    mock index api

--- app/api/api_v1/endpoints/tool.py
+++ app/api/api_v1/endpoints/tool.py
@@ -2,11 +2,11 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from api import deps
-from schemas import RiskIndexSchema
+from schemas import RiskIndexSchema, Message
 
 router = APIRouter()
 
-@router.get("/calculate-risk-index/", response_model=)
+@router.get("/calculate-risk-index/", response_model=Message)
 def calculate_risk_index(
     risk_index_obj: RiskIndexSchema,
     db: Session = Depends(deps.get_db)
@@ -15,3 +15,4 @@ def calculate_risk_index(
     Return risk index associated data for frontend to render.
     """
 
+    return Message(message="a")
