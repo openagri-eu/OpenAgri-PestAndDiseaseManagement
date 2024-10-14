4662ead20bb91c789bd1a1fe5767c6f29e345138
stefan.drobic <stefan.drobic@vizlore.com>
2024-10-14 11:37:22 +0200

    replaced deprecated pydantic Config class with ConfigDict in user schema file

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class UserCreate(BaseModel):
@@ -13,6 +13,4 @@ class UserUpdate(BaseModel):
 class UserMe(BaseModel):
     email: str
 
-    class Config:
-        from_attributes = True
-
+    model_config = ConfigDict(from_attributes=True)
