1a4dece5466ba205bf4a0b497f4b8ab5f342d53d
vlf-stefan-drobic <stefan.drobic@vizlore.com>
2025-01-14 14:02:58 +0100

    Update/gatekeeper integration (#16)
    * changes for gatekeeper, jwt algorithm, service port for service, gatekeeper integration, cors
    
    * updates to apis
    
    * updated readme
    
    * updated with changes to deployment and gatekeeper initialization
    
    * removed prints
    
    * updated service name to use servnice_name var from env

--- app/core/config.py
+++ app/core/config.py
@@ -7,7 +7,7 @@ from os import path, environ
 
 
 class Settings(BaseSettings):
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] | List[str] = None
 
     PROJECT_ROOT: str = path.dirname(path.dirname(path.realpath(__file__)))
 
@@ -45,6 +45,15 @@ class Settings(BaseSettings):
 
     ACCESS_TOKEN_EXPIRATION_TIME: int
     JWT_KEY: str
+    SERVICE_PORT: int
+    JWT_ALGORITHM: str
+
+    # Gatekeeper info
+    USING_GATEKEEPER: bool
+    GATEKEEPER_BASE_URL: Optional[AnyHttpUrl] = None
+    GATEKEEPER_USERNAME: str
+    GATEKEEPER_PASSWORD: str
+    SERVICE_NAME: str
 
 
 settings = Settings()
---
+++ app/init/init_gatekeeper.py
@@ -0,0 +1,50 @@
+import requests
+from fastapi import APIRouter
+
+from core import settings
+
+from api.api_v1.endpoints import operator, pest_model, rule, tool, unit
+
+def register_apis_to_gatekeeper():
+
+    # Login
+    at = requests.post(
+        url=settings.GATEKEEPER_BASE_URL.unicode_string() + "api/login/",
+        headers={"Content-Type": "application/json"},
+        json={"username": "{}".format(settings.GATEKEEPER_USERNAME), "password": "{}".format(settings.GATEKEEPER_PASSWORD)}
+    )
+
+    temp = at.json()
+
+    access = temp["access"]
+    refresh = temp["refresh"]
+
+    # Register APIs
+    apis_to_register = APIRouter()
+    apis_to_register.include_router(operator.router, prefix="/operator")
+    apis_to_register.include_router(pest_model.router, prefix="/pest-model")
+    apis_to_register.include_router(rule.router, prefix="/rule")
+    apis_to_register.include_router(tool.router, prefix="/tool")
+    apis_to_register.include_router(unit.router, prefix="/unit")
+
+    for api in apis_to_register.routes:
+
+        requests.post(
+            url=settings.GATEKEEPER_BASE_URL.unicode_string() + "api/register_service/",
+            headers={"Content-Type": "application/json", "Authorization" : "Bearer {}".format(access)},
+            json={
+                "base_url": "{}:{}".format(settings.SERVICE_NAME, settings.SERVICE_PORT),
+                "service_name": "{}".format(settings.SERVICE_NAME),
+                "endpoint": "api/v1/" + api.path.strip("/"),
+                "methods": list(api.methods)
+            }
+        )
+
+    # Logout
+    requests.post(
+        url=settings.GATEKEEPER_BASE_URL.unicode_string() + "api/logout/",
+        headers={"Content-Type": "application/json"},
+        json={"refresh": refresh}
+    )
+
+    return
