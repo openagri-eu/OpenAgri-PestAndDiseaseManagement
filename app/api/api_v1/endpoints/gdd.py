cb5bf0ed6db77db32dbf99182a753487eb73b7a7
Stefan Drobic <stefan.drobic@vizlore.com>
2025-02-06 16:01:56 +0100

    removed temporary api, accidentally pushed

--- app/api/api_v1/endpoints/gdd.py
+++ app/api/api_v1/endpoints/gdd.py
@@ -19,37 +19,4 @@ def calculate_gdd(
     This API calculates the GDD for a dataset and a pest model
     """
 
-    weather_dataset = crud.dataset.get(db=db, id=weather_dataset_id)
-
-    if not weather_dataset:
-        raise HTTPException(
-            status_code=400,
-            detail="Error, weather dataset with ID:{} does not exist.".format(weather_dataset_id)
-        )
-
-    data_db = crud.data.get_data_query_by_dataset_id(db=db, dataset_id=weather_dataset_id)
-    df = pd.read_sql(sql=data_db.statement, con=db.bind, parse_dates={"date": "%Y-%m-%d"})
-
-    baseline_temp = 11.0
-
-    dates = df["date"].to_list()
-    atdas = df["atmospheric_temperature_daily_average"].to_list()
-    final_list_of_gdds = []
-    global_temp_gdd = 0
-
-    for c in range(len(atdas)):
-        curr_gdd = 0
-
-        if atdas[c] > baseline_temp:
-            curr_gdd = atdas[c] - baseline_temp
-
-        global_temp_gdd = global_temp_gdd + curr_gdd
-
-        final_list_of_gdds.append((dates[c], global_temp_gdd))
-
-    response = {
-        "total_accumulated_gdd": final_list_of_gdds[-1][1],
-        "gdds_accumulated_per_day": map(lambda x: (str(x[0]).split("T")[0].split(" ")[0], x[1]), final_list_of_gdds)
-    }
-
-    return response
+    pass
