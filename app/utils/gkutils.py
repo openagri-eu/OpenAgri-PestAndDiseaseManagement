68ab71d9d5a688c13c0c4ee944a0c60fc155f047
Stefan Drobić <stefan.drobic@vizlore.com>
2025-08-04 02:05:01 +0200

    fixes issues when logging out of the service

--- app/utils/gkutils.py
+++ app/utils/gkutils.py
@@ -10,7 +10,8 @@ def gatekeeper_logout(
 ):
     try:
         response = requests.post(
-            url=settings.GATEKEEPER_BASE_URL + "/api/logout/",
+            url=str(settings.GATEKEEPER_BASE_URL) + "api/logout/",
+            headers={"Content-Type": "application/json"},
             json={
                 "refresh": "{}".format(refresh_token)
             }
@@ -26,3 +27,50 @@ def gatekeeper_logout(
             status_code=400,
             detail="Error, missing refresh token"
         )
+
+    if response.status_code == 500:
+        raise HTTPException(
+            status_code=400,
+            detail="Error, gatekeeper returned a 500!"
+        )
+
+def check_token_for_validity(
+        token: str,
+        token_type: str
+):
+    try:
+        response = requests.post(
+            url=str(settings.GATEKEEPER_BASE_URL) + "api/validate_token/",
+            headers={"Content-Type": "application/json"},
+            json={
+                "token": token,
+                "token_type": token_type # Can be either access or refresh
+            }
+        )
+    except RequestException as re:
+        raise HTTPException(
+            status_code=400,
+            detail="Error, can't connect to gatekeeper instance [{}]".format(re)
+        )
+
+    if response.status_code == 400:
+
+        response_json = response.json()
+
+        if "error" in response_json:
+            error_message = response_json["error"]
+
+            if error_message == "Token is required":
+                raise HTTPException(
+                    status_code=400,
+                    detail="Error, missing token"
+                )
+        return False
+
+    if response.status_code == 500:
+        raise HTTPException(
+            status_code=400,
+            detail="Error, gatekeeper returned a 500"
+        )
+
+    return True
